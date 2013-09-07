b10e560ff32e5b3cd36a3ec80bab7b7b066e4413
GianVizzotto <gfvizzotto@gmail.com>
2013-09-07 15:34:36 -0300

    ajustada a mascara de celular

--- web/root/static/js/forms.js
+++ web/root/static/js/forms.js
@@ -1,7 +1,19 @@
 $( document ).ready(function() {
     $('.date').mask('99/99/9999');
     $('.phone').mask('(99) 9999-9999');
-    $('.mobile_phone').mask('(99) 9999-9999?9');
     $('.postal_code').mask('99999-999');
     $('.cpf').mask('999.999.999-99');
+
+    $('.mobile_phone').focusout(function(){
+        var phone, element;
+        element = $(this);
+        element.unmask();
+        phone = element.val().replace(/\D/g, '');
+        if(phone.length > 10) {
+            element.mask("(99) 99999-9999");
+        } else {
+            element.mask("(99) 9999-9999?9");
+        }
+    }).trigger('focusout');
+
 });
