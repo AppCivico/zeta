1f2ef23aa4c361adaba710b89c7e0772c1f0aa27
renato + shin + gian - maquina de teste <renato e shin // maquina de testes>
2013-09-15 04:01:20 -0300

    layout

--- web/root/static/js/forms.js
+++ web/root/static/js/forms.js
@@ -20,18 +20,18 @@ function re_mask() {
 $( document ).ready(function() {
     re_mask();
 	if ($("#elm_vehicle_body_style_id").length > 0){
-			var body_style_ui = $("<div class='body-style-select'><ul></ul></div>");
-			$("#elm_vehicle_body_style_id option").each(function(index,item){
-				if ($(item).attr("value") != ""){
-					$(body_style_ui).find("ul").append("<li class='item body-style-" + $(item).attr("value") + "'><input type='radio' name='body-style-select' value='" + $(item).attr("value") + "'><div class='image'></div><div class='text'>" + $(item).html() + "</div></li>");
-				}
-			});
-			$("#elm_vehicle_body_style_id").after($(body_style_ui));
-			$("#elm_vehicle_body_style_id").hide();
+		var body_style_ui = $("<div class='body-style-select'><ul></ul></div>");
+		$("#elm_vehicle_body_style_id option").each(function(index,item){
+			if ($(item).attr("value") != ""){
+				var sChecked = ($(item).attr("value") == $("#elm_vehicle_body_style_id").val()) ? "checked='checked'" : "";
+				$(body_style_ui).find("ul").append("<li class='item body-style-" + $(item).attr("value") + "'><input type='radio' name='body-style-select' value='" + $(item).attr("value") + "' " + sChecked + "><div class='image'></div><div class='text'>" + $(item).html() + "</div></li>");
+			}
+		});
+		$("#elm_vehicle_body_style_id").after($(body_style_ui));
+		$("#elm_vehicle_body_style_id").hide();
 
-			$(".body-style-select ul li input").change(function(e){
-				$("#elm_vehicle_body_style_id").val($(this).val());
-			});
-
+		$(".body-style-select ul li input").change(function(e){
+			$("#elm_vehicle_body_style_id").val($(this).val());
+		});
 	}
 });
