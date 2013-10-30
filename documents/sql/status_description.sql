6d4cccca3a039a0008a466df2585d39f77f89f76
GianVizzotto <gfvizzotto@gmail.com>
2013-10-30 18:40:13 -0200

    atualizado dump da tabela de status

--- documents/sql/status_description.sql
+++ documents/sql/status_description.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.1.9
 -- Dumped by pg_dump version 9.1.9
--- Started on 2013-10-24 11:45:58 BRST
+-- Started on 2013-10-30 18:39:50 BRST
 
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
@@ -15,8 +15,8 @@ SET client_min_messages = warning;
 SET search_path = public, pg_catalog;
 
 --
--- TOC entry 2120 (class 0 OID 60645)
--- Dependencies: 241 2121
+-- TOC entry 2129 (class 0 OID 60645)
+-- Dependencies: 241 2130
 -- Data for Name: status_description; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -24,11 +24,13 @@ COPY status_description (id, description) FROM stdin;
 1	Ativo
 2	Inativo
 3	Removido
+4	Em aprovação
+5	Rejeitado
 \.
 
 
 --
--- TOC entry 2125 (class 0 OID 0)
+-- TOC entry 2134 (class 0 OID 0)
 -- Dependencies: 240
 -- Name: status_description_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -36,7 +38,7 @@ COPY status_description (id, description) FROM stdin;
 SELECT pg_catalog.setval('status_description_id_seq', 3, true);
 
 
--- Completed on 2013-10-24 11:45:58 BRST
+-- Completed on 2013-10-30 18:39:50 BRST
 
 --
 -- PostgreSQL database dump complete
