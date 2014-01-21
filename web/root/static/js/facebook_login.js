1b29f77975984b4d405a160ad80227278f03372e
GianVizzotto <gfvizzotto@gmail.com>
2014-01-21 18:27:22 -0200

    Dumping response

--- web/root/static/js/facebook_login.js
+++ web/root/static/js/facebook_login.js
@@ -52,6 +52,7 @@ window.fbAsyncInit = function() {
 function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
+        console.log(response);
         console.log('Good to see you, ' + response.name + '.');
     });
 }
