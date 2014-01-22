85a7ed79a8dfb65dd7583162760405d69ea13d0d
GianVizzotto <gfvizzotto@gmail.com>
2014-01-22 16:46:39 -0200

    Teste fb login

--- web/root/static/js/facebook_login.js
+++ web/root/static/js/facebook_login.js
@@ -14,13 +14,11 @@ window.fbAsyncInit = function() {
     FB.Event.subscribe('auth.authResponseChange', function(response) {
         // Here we specify what we do with the response anytime this event occurs.
         if (response.status === 'connected') {
-            console.log(response);
             // The response object is returned with a status field that lets the app know the current
             // login status of the person. In this case, we're handling the situation where they
             // have logged in to the app.
             testAPI();
         } else if (response.status === 'not_authorized') {
-            console.log(response);
             // In this case, the person is logged into Facebook, but not into the app, so we call
             // FB.login() to prompt them to do so.
             // In real-life usage, you wouldn't want to immediately prompt someone to login
@@ -30,7 +28,6 @@ window.fbAsyncInit = function() {
             // (2) it is a bad experience to be continually prompted to login upon page load.
             FB.login();
         } else {
-            console.log(response);
             // In this case, the person is not logged into Facebook, so we call the login()
             // function to prompt them to do so. Note that at this stage there is no indication
             // of whether they are logged into the app. If they aren't then they'll see the Login
@@ -53,11 +50,14 @@ window.fbAsyncInit = function() {
 // Here we run a very simple test of the Graph API after login is successful.
 // This testAPI() function is only called in those cases.
 function testAPI() {
-
+    console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
         complete_pre_registration(response);
+        console.log(response);
+        console.log('Good to see you, ' + response.name + '.');
     });
 }
+
 
 function complete_pre_registration (response) {
     if(!response) {
