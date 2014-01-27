6d7add99ee503cd83fb141e54668af0565e21d0b
GianVizzotto <gfvizzotto@gmail.com>
2014-01-27 16:45:47 -0200

    Debugando o porque não aparece as mensagens de form error

--- web/lib/WebPI/Controller/Root.pm
+++ web/lib/WebPI/Controller/Root.pm
@@ -49,9 +49,13 @@ sub root : Chained('/') : PathPart('') : CaptureArgs(0) {
 
     $c->stash->{c_req_path} = $c->req->path;
 
+    my $x = $c->session;
+    use Data::Dumper; print STDERR Dumper $x;
     $c->load_status_msgs;
     my $status_msg = $c->stash->{status_msg};
     my $error_msg  = $c->stash->{error_msg};
+use DDP; p $status_msg;
+use DDP; p $error_msg;
 
     @{ $c->stash }{ keys %$status_msg } = values %$status_msg if ref $status_msg eq 'HASH';
     @{ $c->stash }{ keys %$error_msg }  = values %$error_msg  if ref $error_msg eq 'HASH';
