c17d97f17b6b323e9d208f41ef0372a9ac1fa3fc
GianVizzotto <gfvizzotto@gmail.com>
2014-01-31 16:13:05 -0200

    Fazendo um teste sobre permissões extendidas, fb_connect

--- web/root/static/js/facebook_login.js
+++ web/root/static/js/facebook_login.js
@@ -1,39 +1,22 @@
+//Based entirely on facebook developers documentation
 
 window.fbAsyncInit = function() {
     FB.init({
         appId      : '700976226602138',
-        status     : true, // check login status
-        cookie     : true, // enable cookies to allow the server to access the session
-        xfbml      : true  // parse XFBML
+        status     : true,
+        cookie     : true,
+        xfbml      : true
     });
 
-    // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
-    // for any authentication related change, such as login, logout or session refresh. This means that
-    // whenever someone who was previously logged out tries to log in again, the correct case below
-    // will be handled.
     FB.Event.subscribe('auth.authResponseChange', function(response) {
-        // Here we specify what we do with the response anytime this event occurs.
         if (response.status === 'connected') {
-            // The response object is returned with a status field that lets the app know the current
-            // login status of the person. In this case, we're handling the situation where they
-            // have logged in to the app.
-            testAPI(response);
+            connectAPI(response);
         } else if (response.status === 'not_authorized') {
-            // In this case, the person is logged into Facebook, but not into the app, so we call
-            // FB.login() to prompt them to do so.
-            // In real-life usage, you wouldn't want to immediately prompt someone to login
-            // like this, for two reasons:
-            // (1) JavaScript created popup windows are blocked by most browsers unless they
-            // result from direct interaction from people using the app (such as a mouse click)
-            // (2) it is a bad experience to be continually prompted to login upon page load.
-            FB.login();
+            FB.login(function(response) {
+            }, {scope: 'email,user_birthday,user_hometown,user_relationships'});
         } else {
-            // In this case, the person is not logged into Facebook, so we call the login()
-            // function to prompt them to do so. Note that at this stage there is no indication
-            // of whether they are logged into the app. If they aren't then they'll see the Login
-            // dialog right after they log in to Facebook.
-            // The same caveats as above apply to the FB.login() call here.
-            FB.login();
+            FB.login(function(response) {
+            }, {scope: 'email,user_birthday,user_hometown,user_relationships'});
         }
     });
 };
@@ -47,27 +30,28 @@ window.fbAsyncInit = function() {
     ref.parentNode.insertBefore(js, ref);
 }(document));
 
-// Here we run a very simple test of the Graph API after login is successful.
-// This testAPI() function is only called in those cases.
-function testAPI(auth_resp) {
-    processAuth(auth_resp);
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-        complete_pre_registration(response);
-        console.log(response);
-        console.log('Good to see you, ' + response.name + '.');
-    });
-}
+function connectAPI(auth_resp) {
 
+    processAuth(auth_resp);
+
+    FB.api('/me', function(response) {
+
+        complete_pre_registration(response);
+
+        console.log(response);
+    });
+
+}
 
 function complete_pre_registration (response) {
     if(!response) {
         return false;
     }
-
+    console.log(response);
     if(response.name) {
         $('#elm_name').val(response.name);
     }
+
     if(response.gender) {
         if(response.gender == 'male') {
             $('#elm_gender').val('m');
@@ -75,6 +59,7 @@ function complete_pre_registration (response) {
             $('#elm_gender').val('f');
         }
     }
+
 }
 
 function processAuth(auth_resp) {
