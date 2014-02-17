aa94a4faea764ea9d5bd73ef87e511e77fa1c1df
GianVizzotto <gfvizzotto@gmail.com>
2014-02-17 10:01:21 -0300

    Ajustando o repositório

---
+++ api/dbic_dump_local.sh
@@ -0,0 +1,4 @@
+perl script/pi_create.pl model DB DBIC::Schema PI::Schema create=static components=TimeStamp,PassphraseColumn 'dbi:Pg:dbname=web_pi;host=localhost' postgres system quote_names=1 overwrite_modifications=1
+
+rm lib/PI/Model/DB.pm.new;
+rm t/model_DB.t.new;
