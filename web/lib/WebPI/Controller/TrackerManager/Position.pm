ba2b4b57759346b950177d00c22555b3f5760ffd
Gian Vizzotto <gfvizzotto@gmail.com>
2014-05-05 18:08:04 -0300

    Adapted to show tracker positions on a map

--- web/lib/WebPI/Controller/TrackerManager/Position.pm
+++ web/lib/WebPI/Controller/TrackerManager/Position.pm
@@ -6,7 +6,17 @@ use Geo::Coordinates::DecimalDegrees;
 
 BEGIN { extends 'Catalyst::Controller' }
 
-sub base : Chained('/trackermanager/base') : PathPart('positions') : CaptureArgs(0) { }
+sub base : Chained('/trackermanager/base') : PathPart('positions') : CaptureArgs(0) {
+	my ( $self, $c ) = @_;
+	
+	my $api = $c->model('API');
+	
+    $api->stash_result(
+        $c, ['trackers'],
+    );
+    
+    $c->stash->{trackers_obj} = [ map { [ $_->{id}, $_->{code} ] } @{ $c->stash->{trackers} } ];
+}
 
 sub index : Chained('base') : PathPart('') : Args(0) { }
 
@@ -14,47 +24,55 @@ sub get_positions : Chained('/trackermanager/base') : PathPart('get_positions') 
     my ( $self, $c ) = @_;
 
     my $api  = $c->model('API');
-
+    
+    my $params 	= { %{ $c->req->params } };
+    my $form 	= $c->model('Form');
+    
+    $form->format_date($params, 'date');
+    
     $api->stash_result(
         $c,
         ['vehicle_trackers'],
-        params => {
-            positions => 1
-        }
+        params => $params
     );
+    
+    my $t = $c->stash->{vehicle_trackers};
+    
+    use DDP; p $t;
 
-    my $data;
-    my $i   = 0;
-    my $j   = 0;
-    my $id  = $c->stash->{vehicle_trackers}[0]{tracker_id};
+    my @data;
+#     my $i   = 0;
+#     my $j   = 0;
+#     my $id  = $c->stash->{vehicle_trackers}[0]{tracker_id};
 
     foreach my $item ( @{ $c->stash->{vehicle_trackers} } ) {
 
-        if ( $id != $item->{tracker_id} ) {
-            $id = $item->{tracker_id};
-            $i  = 0;
+#         if ( $id != $item->{tracker_id} ) {
+#             $id = $item->{tracker_id};
+#             $i  = 0;
+# 
+#             $j++;
+#         }
 
-            $j++;
-        }
+         my @pos = $self->parse_lat_lng($item->{lat}, $item->{lng});
 
-        my @pos = $self->parse_lat_lng($item->{lat}, $item->{lng});
-
-        $data->{$j}[$i] = {
-            'lat'           => $pos[0],
-            'lng'           => $pos[1],
+        push(@data, {
+             'lat'           => $pos[0],
+             'lng'           => $pos[1],
+#             'lat'           => $item->{lat},
+#             'lng'           => $item->{lng},
             'track_event'   => $item->{track_event},
-            'tracker_id'    => $id,
             'speed'         => $item->{speed},
-        };
+        });
 
-        $i++;
+#         $i++;
     }
 
     $c->res->header( 'content-type', 'application/json;charset=UTF-8' );
 
     if ( $c->stash->{vehicle_trackers} ) {
 
-        $c->res->body( encode_json( $data ) );
+        $c->res->body( encode_json( \@data ) );
     } else {
 
         $c->res->body(0);
